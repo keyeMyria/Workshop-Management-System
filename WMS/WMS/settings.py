5bd281b74762d7f902cbb12d7c72e0d12ce8fc5e
dolphin <wdschn2016@gmail.com>
2018-07-15 22:05:50 +0800

    update settings

--- WMS/WMS/settings.py
+++ WMS/WMS/settings.py
@@ -1,18 +1,11 @@
-
-
+# -*- coding:utf-8 -*-
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'fli=(wbzw+o^_8nxqb1!01*byehw39p%d+@=&mje4(v2&x_h6a'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = []
@@ -67,9 +60,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'WMS.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
@@ -84,10 +74,6 @@ DATABASES = {
 
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -105,8 +91,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/2.0/topics/i18n/
 
 LANGUAGE_CODE = 'zh-hans'
 
@@ -129,8 +113,6 @@ EMPLOYEE_GROUP ={
 
 BROKER_URL = 'redis://localhost:6379/'
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/django-static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
